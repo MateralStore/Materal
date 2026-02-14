e299001d3a2fef2434727db491224d1a4f236392
Materal <cloomcmx1554@hotmail.com>
2026-02-14 09:25:08 +0800

    update: 更新子模块

--- InstallTool.ps1
+++ InstallTool.ps1
@@ -1,3 +1,6 @@
+dotnet tool uninstall -g Materal.Tools.Command
+dotnet tool uninstall -g Materal.Logger.LoggerTrace
+dotnet tool uninstall -g Materal.MergeBlock.Tools
 dotnet tool update -g Materal.Tools.Command
 dotnet tool update -g Materal.Logger.LoggerTrace
 dotnet tool update -g Materal.MergeBlock.Tools
